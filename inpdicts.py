fe09659cf4d78380029b98b6278370ec425184e3
theanishajain <123887402+theanishajain@users.noreply.github.com>
2024-07-04 00:06:47 +0530

    first commit after so long

---
+++ inpdicts.py
@@ -0,0 +1,8 @@
+entry = {}
+num_Of_Entries = int(input("How many number of entries you will do?"))
+for i in range(num_Of_Entries):
+    key = input("Enter your Key: ")
+    value = input("Enter your Value: ")
+    entry[key] = value
+    
+print("Your Dictionary is: ", entry)
